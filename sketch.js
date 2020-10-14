2d51d4010a6e59111b986f1f6d8bcc4ca436b745
sunanthavenkatesan <54566675+sunanthavenkatesan@users.noreply.github.com>
2020-10-14 20:39:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+
+var engine,world,object;
+
+
+function setup() {
+  createCanvas(400,400);
+  engine=Engine.create();
+  world=engine.world;
+ // var object_options={
+   // isStatic:true 
+
+  //}
+object=Bodies.rectangle(200,200,50,50)
+World.add(world,object)
+
+
+console.log(object)
+console.log(object.position.x);
+console.log(object.position.y);
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+  rectMode(CENTER)
+  rect(object.position.x,object.position.y,50,50) 
+  drawSprites();
+}
